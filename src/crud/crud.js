e03e3c64836a3dcf84d2cf28c6883e6493ac9ce8
ben40d9 <benpaley6@gmail.com>
2023-03-07 13:42:00 -0800

    import/export hub for crud functions

---
+++ src/crud/crud.js
@@ -0,0 +1,16 @@
+import { createListing, createMultipleListings } from "./create.js";
+import {
+  listDatabases,
+  findListings,
+  findOneListingByName,
+  findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews,
+} from "./read.js";
+
+export {
+  createListing,
+  createMultipleListings,
+  listDatabases,
+  findListings,
+  findOneListingByName,
+  findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews,
+};

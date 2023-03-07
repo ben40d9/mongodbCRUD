a01c16e8969d573c71eec81f6b622e247ccd8db7
ben40d9 <benpaley6@gmail.com>
2023-03-07 13:41:39 -0800

    read functions exported to the crud import/export home

---
+++ src/crud/read.js
@@ -0,0 +1,103 @@
+//functions to read data on mongodb
+
+//READ ONE DOCUMENT
+//function to findOne (airBnB for now)
+export async function findOneListingByName(client, nameOfListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .findOne({ name: nameOfListing });
+
+  if (result) {
+    console.log(
+      `Found a listing in the collection with the name '${nameOfListing}':`
+    );
+    console.log(result);
+  } else {
+    console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
+
+//list all databases in our cluster and print the results in the console
+export async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+//function to query listings from the sample database
+export async function findListings(client, resultsLimit) {
+  const cursor = client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .find()
+    .limit(resultsLimit);
+
+  const results = await cursor.toArray();
+  if (results.length > 0) {
+    console.log(`Found ${results.length} listing(s):`);
+    results.forEach((result, i) => {
+      const date = new Date(result.last_review).toDateString();
+
+      console.log();
+      console.log(`${i + 1}. name: ${result.name}`);
+      console.log(`   _id: ${result._id}`);
+      console.log(`   bedrooms: ${result.bedrooms}`);
+      console.log(`   bathrooms: ${result.bathrooms}`);
+      console.log(
+        `   most recent review date: ${new Date(
+          result.last_review
+        ).toDateString()}`
+      );
+    });
+  }
+}
+
+export async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(
+  client,
+  {
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER,
+  } = {}
+) {
+  const cursor = client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    //.find() is the query object
+    .find({
+      bedrooms: { $gte: minimumNumberOfBedrooms },
+      bathrooms: { $gte: minimumNumberOfBathrooms },
+    })
+    //we sort the results in descending order (indicated by passing -1 to sort())
+    .sort({ last_review: -1 })
+    //limit() will set the limit for the cursor
+    .limit(maximumNumberOfResults);
+
+  const results = await cursor.toArray();
+
+  if (results.length > 0) {
+    console.log(
+      `Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms:`
+    );
+    results.forEach((result, i) => {
+      const date = new Date(result.last_review).toDateString();
+
+      console.log();
+      console.log(`${i + 1}. name: ${result.name}`);
+      console.log(`   _id: ${result._id}`);
+      console.log(`   bedrooms: ${result.bedrooms}`);
+      console.log(`   bathrooms: ${result.bathrooms}`);
+      console.log(
+        `   most recent review date: ${new Date(
+          result.last_review
+        ).toDateString()}`
+      );
+    });
+  } else {
+    console.log(
+      `No listings found with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`
+    );
+  }
+}
